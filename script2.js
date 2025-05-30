729b62ac737ecafefe2d41f4965b2586e56d06cd
Iamkorir3 <korir5956@gmail.com>
2025-05-30 21:34:21 +0300

    recreating

---
+++ script2.js
@@ -0,0 +1,56 @@
+var addlist = document.getElementById("btncalladders");
+var addsection = document.getElementById("schema");
+var bodyText = document.getElementById("bodytext");
+
+addlist.addEventListener("click", calladders);
+
+function calladders() {
+  addsection.classList.remove("hide_myelements");
+  inputlist.focus();
+  bodyText.classList.add("hide_elements");
+}
+
+let btnaddlist = document.getElementById("btnaddlist");
+let mylist = document.getElementById("mylist");
+let inputlist = document.getElementById("inputlist");
+let taskName = "";
+let newListItem;
+let textNode;
+let btnmyOption;
+let btnmyOptionNode;
+let quickaction = document.getElementById("quickaction_menu");
+let btnquickaction1 = document.getElementById("btn_quickaction1");
+let btnquickactionMAR = document.getElementById("btn_quickaction2");
+let btnquickactionDel = document.getElementById("btn_quickaction3");
+let listID;
+let list;
+let pushList;
+//completed items variables
+let CompletedList = document.getElementById("CompletedList");
+let CompletedIcon = document.getElementById("CompletedIcon");
+let btnCompletedback = document.getElementById("btnCompletedback");
+let CompletedUl = document.getElementById("CompletedUl");
+let openCmtditems = document.getElementById("openCmtditems");
+let newtargetLI;
+let btnmyOptionLI;
+
+const listitems = [];
+
+const createItem = () => {
+  newListItem = document.createElement("li");
+  textNode = document.createTextNode(taskName);
+  btnmyOption = document.createElement("button");
+  btnmyOptionNode = document.createTextNode("⋮");
+  btnmyOption.appendChild(btnmyOptionNode);
+  newListItem.append(textNode, btnmyOption);
+  console.log(newListItem);
+  //   mylist.appendChild(newListItem);
+  return newListItem;
+};
+
+listitems.forEach(createItem);
+
+btnaddlist.onclick = (e) => {
+  taskName = inputlist.value;
+  createItem();
+};
